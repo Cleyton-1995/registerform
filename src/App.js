17fa9f41ffb8fa0c6f0f6015ed238caada78a887
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2024-10-22 08:32:32 -0300

    First Components Functions and Facilitating Prototyping

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import FormularioCadastro from "./Components/Formulario/FormularioCadastro";
 
 function App() {
-  return <h1>Learn React</h1>;
+  return (
+    <>
+      <h1>Learn React</h1>;
+      <FormularioCadastro />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Formulario/FormularioCadastro.jsx
@@ -0,0 +1,26 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+export default function FormularioCadastro() {
+  return (
+    <form>
+      <label>Nome</label>
+      <input type="text" />
+
+      <label>Sobrenome</label>
+      <input type="text" />
+
+      <label>CPF</label>
+      <input type="text" />
+
+      <label>Promoções</label>
+      <input type="checkbox" />
+      <label>Novidades</label>
+      <input type="checkbox" />
+
+      <Button variant="contained" color="primary" type="submit">
+        Cadastrar
+      </Button>
+    </form>
+  );
+}
